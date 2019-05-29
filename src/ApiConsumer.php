bddb70d7b5ebc1341684bf25e35d2b5c83bebdad
Ethan Brace <ethanabrace@gmail.com>
2019-05-29 16:08:50 -0400

    allow for case insensitive calls to endpoints or shapes

--- src/ApiConsumer.php
+++ src/ApiConsumer.php
@@ -11,17 +11,29 @@ abstract class ApiConsumer
     public static function __callStatic($name, $arguments)
     {
         $endpoint = (new \ReflectionClass(get_called_class()))->getNamespaceName() . "\\Endpoints\\" . $name;
+
         if (! class_exists($endpoint)) {
             $endpoint = (new \ReflectionClass(get_called_class()))->getNamespaceName() . "\\Endpoints\\" . $name . "Endpoint";
         }
+
+        if (! class_exists($endpoint)) {
+            $endpoint = (new \ReflectionClass(get_called_class()))->getNamespaceName() . "\\Endpoints\\" . ucfirst($name) . "Endpoint";
+        }
+
         if (! class_exists($endpoint)) {
             throw new \Exception("Class $endpoint does not exist.");
         }
 
         $shape = (new \ReflectionClass(get_called_class()))->getNamespaceName() . "\\Shapes\\" . $name;
+
         if (! class_exists($shape)) {
             $shape = (new \ReflectionClass(get_called_class()))->getNamespaceName() . "\\Shapes\\" . $name . "Shape";
         }
+
+        if (! class_exists($shape)) {
+            $shape = (new \ReflectionClass(get_called_class()))->getNamespaceName() . "\\Shapes\\" . ucfirst($name) . "Shape";
+        }
+
         if (! class_exists($shape)) {
             throw new \Exception("Class $shape does not exist.");
         }
